123f8183817d4363500fa3a5d4811f0b85abfaad
Daniela Nahuelpan <Daniela.paz.n.s@gmail.com>
2023-04-22 12:32:49 -0400

    Modifico smooth scroll al navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-$('a').on('click', function(event) {
-    if (this.hash !== '') {
-    event.preventDefault();
-    var hash = this.hash;
-    $('html, body').animate({
-        scrollTop: $(hash).offset().top
-    }, 800, function(){
-        window.location.hash = hash;
+$(document).ready(function() {
+    $('a.nav-link').click(function(){
+        var target = $(this.hash);
+        $('html, body').active({
+            scrollTop: target.offset().top
+        }, 800);
+        return false;
     });
 });
