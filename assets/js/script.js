8474d33baa4ac6fdc280ebe0cb644202de2ed14d
Daniela Nahuelpan <Daniela.paz.n.s@gmail.com>
2023-04-22 21:03:11 -0400

    Modifico la clase navbar para resolver el problema de la visualización en el movil

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-// $(document).ready(function() {
-//     $('a.nav-link').click(function(){
-//         var target = $(this.hash);
-//         $('html, body').active({
-//             scrollTop: target.offset().top
-//         }, 800);
-//         return false;
-//     });
-// });
+$(document).ready(function() {
+    $('a.nav-link').click(function(){
+        var target = $(this.hash);
+        $('html, body').active({
+            scrollTop: target.offset().top
+        }, 800);
+        return false;
+    });
+});
